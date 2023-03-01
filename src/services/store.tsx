7afcca2646d0c3bd964cfb04e5cf8b3f3d7e4421
Kos Huang <koshuang@gmail.com>
2023-03-02 00:00:37 +0800

    chore: upgrade react and other packages except react-router
    react router v6 改太大，暫時別碰

--- src/services/store.tsx
+++ src/services/store.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import { useContext } from "react";
 import { cookies } from "./fakeData";
 
 const StoreContext = React.createContext<any>({});
 export const useStore = () => useContext(StoreContext);
 
-export const Provider: React.FC = ({ children }) => {
+export const Provider: React.FC<PropsWithChildren> = ({ children }) => {
   const [user, setUser] = useState();
   const [cart, setCart] = useState({ products: [] });
   const [orders, setOrders] = useState([]);
