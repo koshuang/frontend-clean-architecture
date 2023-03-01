00cd99581c8e3df07c01da98432995b80e8a4562
Kos Huang <koshuang@gmail.com>
2023-03-02 00:47:00 +0800

    chore: move user component into cart module

--- src/modules/app/infrastructure/ui/components/App.tsx
+++ src/modules/app/infrastructure/ui/components/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Auth } from "../../../../auth/infrastructure/ui/components/Auth";
-import { User } from "../../../../../ui/User";
+import { User } from "../../../../cart/infrastructure/ui/components/User";
 import { Front } from "../../../../front/infrastructure/ui/components/Front";
 
 import { Header } from "./Header";
---
+++ src/modules/cart/infrastructure/ui/components/User/User.tsx
@@ -0,0 +1,20 @@
+import { Redirect } from "react-router";
+import { useUserStorage } from "../../../../../../adapters/storageAdapter";
+import { Buy } from "../../../../../order/infrastructure/ui/components/Buy";
+import { Cart } from "../Cart";
+import { Orders } from "../../../../../order/infrastructure/ui/components/Orders";
+import { Profile } from "../../../../../core/infrastructure/ui/components/Profile";
+
+export function User() {
+  const { user } = useUserStorage();
+  if (!user) return <Redirect to="/auth" />;
+
+  return (
+    <main>
+      <Profile />
+      <Orders />
+      <Cart />
+      <Buy />
+    </main>
+  );
+}
